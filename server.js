644d499e585f57e7e12c849434659b8efb316a8b
Amit Deka <amitdeka49@gmail.com>
2023-04-16 22:58:21 +0530

    Still Trying

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const app = express();
 // Static folder
 app.use(express.static("public"));
 
-app.listen(process.env.PORT || 3000);
-
 app.use("/", require("./route/routeClient"));
 app.use("/admin", require("./route/routeAdmin"));
 
+app.listen(process.env.PORT || 3000);
 module.exports = app;
