62393eb29eb1e99dc980e812d9449b55ec1b6a6f
Amit Deka <amitdeka49@gmail.com>
2023-04-19 19:20:57 +0530

    Test Deploy

--- controller/controllerAdmin.js
+++ controller/controllerAdmin.js
@@ -1,16 +1,7 @@
+const path = require("path");
+
 const websiteSetup = async (req, res) => {
-  //   try {
-  //     var settings = await websiteSetting.find({});
-  //     if (settings.length > 0) {
-  //       res.redirect("/admin/login");
-  //     } else {
-  //       res.render("admin/website-setup", { title: "Setup" });
-  //     }
-  //   } catch (error) {
-  //     console.log(error.message);
-  //   }
-  //   res.sendFile("admin/index.html", { title: "Setup" });
-  res.sendFile("/admin/index.html", { root: "." });
+  res.sendFile(path.join(__dirname, "../", "admin", "index.html"));
 };
 
 module.exports = {
--- controller/controllerClient.js
+++ controller/controllerClient.js
@@ -1,16 +1,7 @@
+const path = require("path");
+
 const viewHome = async (req, res) => {
-  //   try {
-  //     var settings = await websiteSetting.find({});
-  //     if (settings.length > 0) {
-  //       res.redirect("/admin/login");
-  //     } else {
-  //       res.render("admin/website-setup", { title: "Setup" });
-  //     }
-  //   } catch (error) {
-  //     console.log(error.message);
-  //   }
-  //   res.sendFile("admin/index.html", { title: "Setup" });
-  res.sendFile("/client/index.html", { root: "." });
+  res.sendFile(path.join(__dirname, "../", "client", "index.html"));
 };
 
 module.exports = {
